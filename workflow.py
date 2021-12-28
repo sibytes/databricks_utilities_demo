0fdd301ddfe799ec0bf583b4fa98badcb7e9246e
shaunryan <shaunryan>
2021-12-28 15:14:28 +0000

    testing

---
+++ workflow.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+import discover_modules
+discover_modules.go(spark)
+
+# COMMAND ----------
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+from utilities.workflows import(
+  Notebook,
+  execute_notebooks
+)
+
+
+# build a list of notebooks to run
+task_root = "./workflow_tasks/"
+notebooks = [
+  Notebook(f"{task_root}pyTask1", 3600, {"waittimeout": 15}, 0, True),
+  Notebook(f"{task_root}pyTask2", 3600, {"waittimeout": 10}, 0, True),
+  Notebook(f"{task_root}pyTask3", 3600, {"waittimeout": 8},  0, True),
+  Notebook(f"{task_root}pyTask4", 3600, {"waittimeout": 6},  0, True),
+  Notebook(f"{task_root}pyTask5", 3600, {"waittimeout": 4},  0, True),
+  Notebook(f"{task_root}pyTask6", 3600, {"waittimeout": 0},  0, True)
+]
+
+# execute the notebooks in parallel
+results = execute_notebooks(notebooks, 4, dbutils)
+
+# show the results
+logging.info(results)
+
---
+++ workflow_tasks/pyTask3.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+# dbutils.widgets.removeAll()
+dbutils.widgets.text("waittimeout", "0", "WaitTimout(s)")
+dbutils.widgets.text("notebook", "./pyTask2", "notebook")
+
+# COMMAND ----------
+
+import time
+import json
+
+try:
+  seconds = int(dbutils.widgets.get("waittimeout"))
+  notebook = dbutils.widgets.get("notebook")
+  print(f"Sleeping for {seconds} seconds...")
+  time.sleep(seconds)
+except Exception as e:
+  out = {
+    "status" : "failed",
+    "error" : str(e),
+    "notebook" : notebook
+  }
+  dbutils.notebook.exit(json.dumps(out))
+
+# COMMAND ----------
+
+
+out = json.dumps({
+  "status" : "succeeded",
+  "notebook" : notebook})
+
+dbutils.notebook.exit(out)
